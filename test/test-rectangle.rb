97483e0eebc1abdeaafc75310a8472bd7a8be7ac
Wataru Shimizu <waruzilla@gmail.com>
2020-02-05 20:49:20 +0900

    Add functions corresponding to cv::clipLine()

--- test/test-rectangle.rb
+++ test/test-rectangle.rb
@@ -22,4 +22,36 @@ class TestRectangle < Test::Unit::TestCase
                    ])
     end
   end
+
+  sub_test_case("#clip_line") do
+    def test_true
+      rectangle = CV::Rectangle.new(10, 10, 80, 80)
+      pt1 = CV::Point.new(0, 10)
+      pt2 = CV::Point.new(100, 90)
+      ret, new_pt1, new_pt2 = rectangle.clip_line(pt1, pt2)
+      assert_equal([true, 10, 17, 89, 81],
+                   [
+                     ret,
+                     new_pt1.x,
+                     new_pt1.y,
+                     new_pt2.x,
+                     new_pt2.y
+                   ])
+    end
+
+    def test_false
+      rectangle = CV::Rectangle.new(10, 10, 80, 80)
+      pt1 = CV::Point.new(200, 200)
+      pt2 = CV::Point.new(300, 300)
+      ret, new_pt1, new_pt2 = rectangle.clip_line(pt1, pt2)
+      assert_equal([false, 200, 200, 300, 300],
+                   [
+                     ret,
+                     new_pt1.x,
+                     new_pt1.y,
+                     new_pt2.x,
+                     new_pt2.y
+                   ])
+    end
+  end
 end
--- test/test-size.rb
+++ test/test-size.rb
@@ -18,4 +18,36 @@ class TestSize < Test::Unit::TestCase
                    ])
     end
   end
+
+  sub_test_case("#clip_line") do
+    def test_true
+      size = CV::Size.new(100, 100)
+      pt1 = CV::Point.new(-10, 0)
+      pt2 = CV::Point.new(110, 100)
+      ret, new_pt1, new_pt2 = size.clip_line(pt1, pt2)
+      assert_equal([true, 0, 8, 99, 91],
+                   [
+                     ret,
+                     new_pt1.x,
+                     new_pt1.y,
+                     new_pt2.x,
+                     new_pt2.y
+                    ])
+    end
+
+    def test_false
+      size = CV::Size.new(100, 100)
+      pt1 = CV::Point.new(200, 200)
+      pt2 = CV::Point.new(300, 300)
+      ret, new_pt1, new_pt2 = size.clip_line(pt1, pt2)
+      assert_equal([false, 200, 200, 300, 300],
+                   [
+                     ret,
+                     new_pt1.x,
+                     new_pt1.y,
+                     new_pt2.x,
+                     new_pt2.y
+                    ])
+    end
+  end
 end
