24c9655e239ee0c34528fafb8607366a2a8a36cd
Sutou Kouhei <kou@clear-code.com>
2020-02-21 11:33:06 +0900

    Adjust style

--- opencv-glib/rectangle.h
+++ opencv-glib/rectangle.h
@@ -26,6 +26,8 @@ gint gcv_rectangle_get_width(GCVRectangle *rectangle);
 void gcv_rectangle_set_width(GCVRectangle *rectangle, gint width);
 gint gcv_rectangle_get_height(GCVRectangle *rectangle);
 void gcv_rectangle_set_height(GCVRectangle *rectangle, gint height);
-gboolean gcv_rectangle_clip_line(GCVRectangle* rectangle, GCVPoint **point1, GCVPoint **point2);
+gboolean gcv_rectangle_clip_line(GCVRectangle *rectangle,
+                                 GCVPoint **point1,
+                                 GCVPoint **point2);
 
 G_END_DECLS
